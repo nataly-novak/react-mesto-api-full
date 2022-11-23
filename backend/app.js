45c87e4ebb2a7ddf82766f56ff5ee5d9a9d2c2ad
Nataly Novak <miss.novak137@yandex.com>
2022-11-23 16:15:41 +0300

    work with new api

--- backend/app.js
+++ backend/app.js
@@ -16,17 +16,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-// Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
-  'http://mesto.novak.nomoredomains.club/*',
-  'http://api.mesto.novak.nomoredomains.club/*',
+  'http://mesto.novak.nomoredomains.club',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
