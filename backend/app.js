ffb483c2fed230bf8d63328d038876d6f595fc6e
Nataly Novak <miss.novak137@yandex.com>
2022-11-23 16:25:52 +0300

    work with new api

--- backend/app.js
+++ backend/app.js
@@ -26,19 +26,20 @@ app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
+    console.log("here")
     res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+    const requestHeaders = req.headers['access-control-request-headers'];
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+    }
   }
   next();
 });
