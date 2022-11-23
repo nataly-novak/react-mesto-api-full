ce9855a38f5ec4962f6a0dd02f2cdf116584b58e
Nataly Novak <miss.novak137@yandex.com>
2022-11-23 16:05:19 +0300

    work with new api

--- backend/app.js
+++ backend/app.js
@@ -20,17 +20,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
-  'http://mesto.novak.nomoredomains.club/signup',
+  'http://mesto.novak.nomoredomains.club/*',
+  'http://api.mesto.novak.nomoredomains.club/*',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
   // проверяем, что источник запроса есть среди разрешённых
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
-  res.header('Access-Control-Allow-Origin', '*');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   const requestHeaders = req.headers['access-control-request-headers'];
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
